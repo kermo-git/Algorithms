2e0417db7dd3ca2714c161a5fe1f05d7470128cc
Kermo Saarse <saarse27@gmail.com>
2026-01-21 13:38:00 +0200

    Reorganized code according to newspaper principle

--- src/views/VoronoiCells/WaveFunctionCollapse.ts
+++ src/views/VoronoiCells/WaveFunctionCollapse.ts
@@ -3,6 +3,35 @@
 
 import { Matrix } from '@/utils/Matrix'
 
+export function generate(
+    sample_picture: Matrix<number>,
+    n_rows: number,
+    n_cols: number,
+): Matrix<number[]> {
+    const rules = learn(sample_picture)
+    const state = new State(rules, n_rows, n_cols)
+
+    let collapse_pos = {
+        x: Math.floor(Math.random() * n_rows),
+        y: Math.floor(Math.random() * n_cols),
+    }
+    while (!state.isFinished()) {
+        collapse_pos = collapseAndPropagate(state, collapse_pos)
+    }
+    return state.wave_function
+}
+
+export function flatten(wave_function: Matrix<number[]>): Int32Array {
+    const n_rows = wave_function.n_rows
+    const n_cols = wave_function.n_cols
+    const result = new Int32Array(n_rows * n_cols)
+
+    wave_function.foreach((row, col, superposition) => {
+        result[row * n_cols + col] = superposition.length > 0 ? superposition[0] : 0
+    })
+    return result
+}
+
 enum Direction {
     North = 0,
     NorthEast = 1,
@@ -14,21 +43,114 @@ enum Direction {
     NorthWest = 7,
 }
 
+function learn(sample_picture: Matrix<number>): Rules {
+    let n_tiles = 0
+
+    sample_picture.foreach((row, col, tile) => {
+        if (tile > n_tiles) {
+            n_tiles = tile
+        }
+    })
+    const rules = new Rules(n_tiles)
+
+    const max_row = sample_picture.n_rows - 1
+    const max_col = sample_picture.n_cols - 1
+
+    sample_picture.foreach((row, col, tile) => {
+        rules.addWeight(tile, 1)
+
+        const north_row = row < max_row ? row + 1 : 0
+        const east_col = col < max_col ? col + 1 : 0
+        const south_row = row > 0 ? row - 1 : max_row
+        const west_col = col > 0 ? col - 1 : max_col
+
+        const north_tile = sample_picture.get(north_row, col)
+        const north_east_tile = sample_picture.get(north_row, east_col)
+        const east_tile = sample_picture.get(row, east_col)
+        const south_east_tile = sample_picture.get(south_row, east_col)
+        const south_tile = sample_picture.get(south_row, col)
+        const south_west_tile = sample_picture.get(south_row, west_col)
+        const west_tile = sample_picture.get(row, west_col)
+        const north_west_tile = sample_picture.get(north_row, west_col)
+
+        rules.set(tile, Direction.North, north_tile)
+        rules.set(tile, Direction.NorthEast, north_east_tile)
+        rules.set(tile, Direction.East, east_tile)
+        rules.set(tile, Direction.SouthEast, south_east_tile)
+        rules.set(tile, Direction.South, south_tile)
+        rules.set(tile, Direction.SouthWest, south_west_tile)
+        rules.set(tile, Direction.West, west_tile)
+        rules.set(tile, Direction.NorthWest, north_west_tile)
+    })
+
+    return rules
+}
+
 interface Vec2 {
     x: number
     y: number
 }
 
-const direction_vectors = [
-    { x: 0, y: 1 }, // North
-    { x: 1, y: 1 }, // NorthEast
-    { x: 1, y: 0 }, // East
-    { x: 1, y: -1 }, // SouthEast
-    { x: 0, y: -1 }, // South
-    { x: -1, y: -1 }, // SouthWest
-    { x: -1, y: 0 }, // West
-    { x: -1, y: 1 }, // NorthWest
-]
+function collapseAndPropagate(state: State, start_pos: Vec2) {
+    const n_rows = state.n_rows
+    const n_cols = state.n_cols
+
+    let min_entropy = Number.MAX_VALUE
+    let min_entropy_pos = start_pos
+
+    state.collapse(start_pos)
+
+    function process(x: number, y: number, directions: Direction[]) {
+        const { n_tiles, entropy } = state.propagate({ x, y }, directions)
+
+        if (n_tiles > 1 && entropy < min_entropy) {
+            min_entropy = entropy
+            min_entropy_pos = {
+                x: x % n_cols,
+                y: y % n_rows,
+            }
+        }
+    }
+    const NORTH_SOUTH = [Direction.North, Direction.South]
+    const EAST_SIDE = [Direction.NorthEast, Direction.East, Direction.SouthEast]
+    const WEST_SIDE = [Direction.SouthWest, Direction.West, Direction.NorthWest]
+
+    const BOTH_SIDES = EAST_SIDE.concat(WEST_SIDE)
+    const ALL_SIDES = BOTH_SIDES.concat(NORTH_SOUTH)
+    const L_SHAPE = WEST_SIDE.concat([Direction.South])
+    const C_SHAPE = WEST_SIDE.concat(NORTH_SOUTH)
+    const U_SHAPE = BOTH_SIDES.concat([Direction.South])
+
+    const y_start = start_pos.y + 1
+    const y_end = start_pos.y + n_rows - 2
+    const y_connect = y_end + 1
+
+    const x_start = start_pos.x + 1
+    const x_end = start_pos.x + n_cols - 2
+    const x_connect = x_end + 1
+
+    for (let y = y_start; y <= y_end; y++) {
+        process(start_pos.x, y, [Direction.South])
+    }
+    process(start_pos.x, y_connect, NORTH_SOUTH)
+
+    for (let x = x_start; x <= x_end; x++) {
+        process(x, start_pos.y, WEST_SIDE)
+
+        for (let y = y_start; y <= y_end; y++) {
+            process(x, y, L_SHAPE)
+        }
+        process(x, y_connect, C_SHAPE)
+    }
+    process(x_connect, start_pos.y, BOTH_SIDES)
+
+    for (let y = y_start; y <= y_end; y++) {
+        process(x_connect, y, U_SHAPE)
+    }
+    process(x_connect, y_connect, ALL_SIDES)
+
+    return min_entropy_pos
+}
 
 class Rules {
     weights: number[]
@@ -96,6 +218,17 @@ class Rules {
     }
 }
 
+const direction_vectors = [
+    { x: 0, y: 1 }, // North
+    { x: 1, y: 1 }, // NorthEast
+    { x: 1, y: 0 }, // East
+    { x: 1, y: -1 }, // SouthEast
+    { x: 0, y: -1 }, // South
+    { x: -1, y: -1 }, // SouthWest
+    { x: -1, y: 0 }, // West
+    { x: -1, y: 1 }, // NorthWest
+]
+
 class State {
     rules: Rules
     n_rows: number
@@ -158,137 +291,4 @@ class State {
     isFinished() {
         return !this.is_success || this.n_collapsed === this.n_rows * this.n_cols
     }
-}
-
-function learn(sample_picture: Matrix<number>): Rules {
-    let n_tiles = 0
-
-    sample_picture.foreach((row, col, tile) => {
-        if (tile > n_tiles) {
-            n_tiles = tile
-        }
-    })
-    const rules = new Rules(n_tiles)
-
-    const max_row = sample_picture.n_rows - 1
-    const max_col = sample_picture.n_cols - 1
-
-    sample_picture.foreach((row, col, tile) => {
-        rules.addWeight(tile, 1)
-
-        const north_row = row < max_row ? row + 1 : 0
-        const east_col = col < max_col ? col + 1 : 0
-        const south_row = row > 0 ? row - 1 : max_row
-        const west_col = col > 0 ? col - 1 : max_col
-
-        const north_tile = sample_picture.get(north_row, col)
-        const north_east_tile = sample_picture.get(north_row, east_col)
-        const east_tile = sample_picture.get(row, east_col)
-        const south_east_tile = sample_picture.get(south_row, east_col)
-        const south_tile = sample_picture.get(south_row, col)
-        const south_west_tile = sample_picture.get(south_row, west_col)
-        const west_tile = sample_picture.get(row, west_col)
-        const north_west_tile = sample_picture.get(north_row, west_col)
-
-        rules.set(tile, Direction.North, north_tile)
-        rules.set(tile, Direction.NorthEast, north_east_tile)
-        rules.set(tile, Direction.East, east_tile)
-        rules.set(tile, Direction.SouthEast, south_east_tile)
-        rules.set(tile, Direction.South, south_tile)
-        rules.set(tile, Direction.SouthWest, south_west_tile)
-        rules.set(tile, Direction.West, west_tile)
-        rules.set(tile, Direction.NorthWest, north_west_tile)
-    })
-
-    return rules
-}
-
-function collapseAndPropagate(state: State, start_pos: Vec2) {
-    const n_rows = state.n_rows
-    const n_cols = state.n_cols
-
-    let min_entropy = Number.MAX_VALUE
-    let min_entropy_pos = start_pos
-
-    state.collapse(start_pos)
-
-    function process(x: number, y: number, directions: Direction[]) {
-        const { n_tiles, entropy } = state.propagate({ x, y }, directions)
-
-        if (n_tiles > 1 && entropy < min_entropy) {
-            min_entropy = entropy
-            min_entropy_pos = {
-                x: x % n_cols,
-                y: y % n_rows,
-            }
-        }
-    }
-    const NORTH_SOUTH = [Direction.North, Direction.South]
-    const EAST_SIDE = [Direction.NorthEast, Direction.East, Direction.SouthEast]
-    const WEST_SIDE = [Direction.SouthWest, Direction.West, Direction.NorthWest]
-
-    const BOTH_SIDES = EAST_SIDE.concat(WEST_SIDE)
-    const ALL_SIDES = BOTH_SIDES.concat(NORTH_SOUTH)
-    const L_SHAPE = WEST_SIDE.concat([Direction.South])
-    const C_SHAPE = WEST_SIDE.concat(NORTH_SOUTH)
-    const U_SHAPE = BOTH_SIDES.concat([Direction.South])
-
-    const y_start = start_pos.y + 1
-    const y_end = start_pos.y + n_rows - 2
-    const y_connect = y_end + 1
-
-    const x_start = start_pos.x + 1
-    const x_end = start_pos.x + n_cols - 2
-    const x_connect = x_end + 1
-
-    for (let y = y_start; y <= y_end; y++) {
-        process(start_pos.x, y, [Direction.South])
-    }
-    process(start_pos.x, y_connect, NORTH_SOUTH)
-
-    for (let x = x_start; x <= x_end; x++) {
-        process(x, start_pos.y, WEST_SIDE)
-
-        for (let y = y_start; y <= y_end; y++) {
-            process(x, y, L_SHAPE)
-        }
-        process(x, y_connect, C_SHAPE)
-    }
-    process(x_connect, start_pos.y, BOTH_SIDES)
-
-    for (let y = y_start; y <= y_end; y++) {
-        process(x_connect, y, U_SHAPE)
-    }
-    process(x_connect, y_connect, ALL_SIDES)
-
-    return min_entropy_pos
-}
-
-export function generate(
-    sample_picture: Matrix<number>,
-    n_rows: number,
-    n_cols: number,
-): Matrix<number[]> {
-    const rules = learn(sample_picture)
-    const state = new State(rules, n_rows, n_cols)
-
-    let collapse_pos = {
-        x: Math.floor(Math.random() * n_rows),
-        y: Math.floor(Math.random() * n_cols),
-    }
-    while (!state.isFinished()) {
-        collapse_pos = collapseAndPropagate(state, collapse_pos)
-    }
-    return state.wave_function
-}
-
-export function flatten(wave_function: Matrix<number[]>): Int32Array {
-    const n_rows = wave_function.n_rows
-    const n_cols = wave_function.n_cols
-    const result = new Int32Array(n_rows * n_cols)
-
-    wave_function.foreach((row, col, superposition) => {
-        result[row * n_cols + col] = superposition.length > 0 ? superposition[0] : 0
-    })
-    return result
 }
