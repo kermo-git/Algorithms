48c3f1277911f23cb03683907b95a7c717c2e44f
Kermo Saarse <saarse27@gmail.com>
2026-01-27 15:53:53 +0200

    Restoring code editor caret position after applying syntax highlighting

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -73,15 +73,20 @@ function getCaretOffset(el: HTMLElement): number {
     const range = selection.getRangeAt(0)
     let offset = 0
 
-    function walk(node: Node) {
-        if (node.nodeName === 'BR' || node.nodeName === 'DIV') {
-            offset += 1
-        }
-        if (node === range.endContainer) {
-            offset += range.endOffset
-            return true
-        } else if (node.nodeType === Node.TEXT_NODE) {
-            offset += (node as Text).length
+    function walk(node: Node, is_root = false) {
+        if (!is_root) {
+            if (node.nodeType === Node.TEXT_NODE) {
+                if (node === range.endContainer) {
+                    offset += range.endOffset
+                    return true
+                }
+                offset += (node as Text).length
+            } else if (node.nodeName === 'BR' || node.nodeName === 'DIV') {
+                offset += 1
+                if (node === range.endContainer) {
+                    return true
+                }
+            }
         }
         for (const child of node.childNodes) {
             if (walk(child)) {
@@ -90,7 +95,7 @@ function getCaretOffset(el: HTMLElement): number {
         }
         return false
     }
-    walk(el)
+    walk(el, true)
     return offset
 }
 
@@ -98,22 +103,24 @@ function setCaretOffset(el: HTMLElement, offset: number) {
     const range = document.createRange()
     let current_offset = 0
 
-    function walk(node: Node) {
-        if (node.nodeType === Node.TEXT_NODE) {
-            const next_offset = current_offset + (node as Text).length
+    function walk(node: Node, is_root = false) {
+        if (!is_root) {
+            if (node.nodeType === Node.TEXT_NODE) {
+                const next_offset = current_offset + (node as Text).length
 
-            if (offset <= next_offset) {
-                range.setStart(node, offset - current_offset)
-                range.collapse(true)
-                return true
-            }
-            current_offset = next_offset
-        } else if (node.nodeName === 'BR' || node.nodeName === 'DIV') {
-            current_offset += 1
-            if (offset === current_offset) {
-                range.setStartAfter(node)
-                range.collapse(true)
-                return true
+                if (offset <= next_offset) {
+                    range.setStart(node, offset - current_offset)
+                    range.collapse(true)
+                    return true
+                }
+                current_offset = next_offset
+            } else if (node.nodeName === 'BR' || node.nodeName === 'DIV') {
+                current_offset += 1
+                if (offset === current_offset) {
+                    range.setStartAfter(node)
+                    range.collapse(true)
+                    return true
+                }
             }
         }
         for (const child of node.childNodes) {
@@ -123,7 +130,7 @@ function setCaretOffset(el: HTMLElement, offset: number) {
         }
         return false
     }
-    walk(el)
+    walk(el, true)
 
     const selection = window.getSelection()!
     selection.removeAllRanges()
