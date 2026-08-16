e859948ee1e4a4214b7e08546728221cdad3b1ab
Kermo Saarse <saarse27@gmail.com>
2026-08-16 14:23:08 +0300

    Fix: uniform distribution of states for 2D cellular automata initialization

--- src/views/DiscreteCellularAutomata2D/Scene.ts
+++ src/views/DiscreteCellularAutomata2D/Scene.ts
@@ -142,7 +142,7 @@ export class AutomatonScene {
     reset() {
         const n_cells = this.setup.canvas_width * this.canvas_height
         const random_data = new Uint32Array(n_cells).map(() =>
-            Math.round(Math.random() * (this.setup.n_states - 1))
+            Math.floor(Math.random() * this.setup.n_states)
         )
         this.engine.updateBuffer(this.generation_A, random_data)
         this.generation_A_is_current = true
